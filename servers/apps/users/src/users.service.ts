5fecc792754df66652d7080d744b0079dbd0e42e
Darshan Chobarkar <dchobarkar@gmail.com>
2024-06-06 10:38:29 +0530

    add user services and resolver

--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,8 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+import { LoginDto, RegisterDto } from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  // Register User
+  async register(registerDto: RegisterDto) {
+    const { name, email, password } = registerDto;
+
+    const user = {
+      name,
+      email,
+      password,
+    };
+
+    return user;
+  }
+
+  // Login Service
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    const user = {
+      email,
+      password,
+    };
+
+    return user;
+  }
+
+  // Get all users service
+  async getUsers() {
+    const users = [
+      {
+        id: '1234',
+        name: 'aa',
+        email: 'aa@gmail.com',
+        password: 'password',
+      },
+    ];
+
+    return users;
   }
 }
