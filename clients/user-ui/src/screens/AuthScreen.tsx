860cc3d57ce67cfe7b99becf45f9545e9c8beb1e
Darshan Chobarkar <dchobarkar@gmail.com>
2024-06-14 12:31:30 +0530

    add user verification modal

--- clients/user-ui/src/screens/AuthScreen.tsx
+++ clients/user-ui/src/screens/AuthScreen.tsx
@@ -2,15 +2,19 @@ import React, { useState } from "react";
 
 import Login from "../shared/Auth/Login";
 import Signup from "../shared/Auth/Signup";
+import Verification from "../shared/Auth/Verification";
 
 const AuthScreen = () => {
-  const [activeState, setActiveState] = useState("Login");
+  const [activeState, setActiveState] = useState("Verification");
 
   return (
     <div className="w-full fixed top-0 left-0 h-screen z-50 flex items-center justify-center bg-[#00000027]">
       <div className="w-[450px] bg-slate-900 rounded shadow-sm p-3">
         {activeState === "Login" && <Login setActiveState={setActiveState} />}
         {activeState === "Signup" && <Signup setActiveState={setActiveState} />}
+        {activeState === "Verification" && (
+          <Verification setActiveState={setActiveState} />
+        )}
       </div>
     </div>
   );
