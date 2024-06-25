1b1400fbe7d871f372ab8488cf83eef52348e290
Darshan Chobarkar <dchobarkar@gmail.com>
2024-06-25 12:04:44 +0530

    update hooks

--- apps/restaurant-dashboard/src/hooks/useRestaurant.ts
+++ apps/restaurant-dashboard/src/hooks/useRestaurant.ts
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import Cookies from "js-cookie";
 import { useQuery } from "@apollo/client";
 
-import { GET_RESTAURANT } from "../graphql/actions/get.restaurant";
+import { GET_RESTAURANT } from "../graphql/actions/get-restaurant.action";
 
 const useRestaurant = () => {
   const { loading, data } = useQuery(GET_RESTAURANT);
@@ -12,12 +12,12 @@ const useRestaurant = () => {
   useEffect(() => {
     if (
       !loading &&
-      data?.getLoggedInRestaurant?.restaurant.refreshToken &&
-      data?.getLoggedInRestaurant?.restaurant.accessToken
+      data?.getLoggedInRestaurant?.restaurant.refresh_token &&
+      data?.getLoggedInRestaurant?.restaurant.access_token
     ) {
       const restaurantData = data?.getLoggedInRestaurant;
-      Cookies.set("access_token", restaurantData.accessToken);
-      Cookies.set("refresh_token", restaurantData.refreshToken);
+      Cookies.set("access_token", restaurantData.access_token);
+      Cookies.set("refresh_token", restaurantData.refresh_token);
     }
   }, [data?.getLoggedInRestaurant, loading]);
 
